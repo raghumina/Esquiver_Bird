70d55cf56ac17c3e410cda8d5f9ac89f0acf3480
raghu <raghumina11@gmail.com>
2021-01-22 11:22:12 +0530

    Update Planes.py

--- Planes.py
+++ Planes.py
@@ -6,3 +6,10 @@ import sys
 import pygame
 import pygame.locals
 from pygame import *
+
+# Global variable for games
+FPS = 32  # FRAMES PER SECOND  FOR IMAGE REINDERSCREEN
+SCREENWIDTH = 289
+SCREENHEIGHT = 511
+
+pygame.display.set_mode(SCREENWIDTH, SCREENHEIGHT)
