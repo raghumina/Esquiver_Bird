666999b8decbcba29b297e3ca2de2ab0825b4e5b
raghu <raghumina11@gmail.com>
2021-01-22 14:05:39 +0530

    Update Planes.py

--- Planes.py
+++ Planes.py
@@ -12,4 +12,17 @@ FPS = 32  # FRAMES PER SECOND  FOR IMAGE REINDERSCREEN
 SCREENWIDTH = 289
 SCREENHEIGHT = 511
 
-pygame.display.set_mode(SCREENWIDTH, SCREENHEIGHT)
+pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+GROUNDY = SCREENHEIGHT * 0.8
+GAME_SPRITES = {
+
+}
+GAME_SOUNDS = {}
+PLAYER = "bird.png"
+BACKGROUND = "background.png"
+PIPE = "pipe.png"
+
+if __name__ == "__main__":
+    
+
+
