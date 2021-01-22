75ee83a28e400a016c321d781ed8242a8fc6541f
raghu <raghumina11@gmail.com>
2021-01-22 11:16:52 +0530

    Update Planes.py

---
+++ Planes.py
@@ -0,0 +1,8 @@
+# Plane game inspired form falppy bird
+
+
+import random  # to generate random numbers/ creating random names in games
+import sys
+import pygame
+import pygame.locals
+from pygame import *
